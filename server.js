08fee29b66cb2bbade3148cfc7c4ffaf47b48f7a
Miguel Fidelis <miguelfidelis10@outlook.com>
2025-07-05 01:43:58 -0300

    Chore: Adição do Sentry

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
-const app = require('./src/app');
+const app = require('./app');
+const Sentry = require('./src/config/sentry');
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
-
-
-
-
+app.use(Sentry.Handlers.errorHandler());    
